8e368ed2d166102a95926c2ed899b7d1eaf41cfc
Kenneth Tyler Woodby <kwoodby@uab.edu>
2022-09-10 20:07:21 -0400

    bring in cors and tools

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+// GET /users
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+// POST /users
+
+router.post('/', function(req, res, next) {
+  res.send('THIS IS A POST REQUEST');
 });
 
 module.exports = router;
